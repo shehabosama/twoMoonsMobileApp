eef5a3236bee395b64822995774371f1ee8e077d
shehab <shehabosama0122421@gmail.com>
2020-03-25 08:07:34 +0200

    first commit

--- app/src/main/java/com/android/two_moons_project/common/HelperStuffs/UploadCallBacks.java
+++
@@ -1,6 +0,0 @@
-package com.android.two_moons_project.common.HelperStuffs;
-
-public interface UploadCallBacks {
-    void onProgressUpdate(int percetage);
-
-}
--- app/src/main/java/com/android/two_moons_project/common/base/BasePresenter.java
+++
@@ -1,7 +0,0 @@
-package com.android.two_moons_project.common.base;
-
-/**
- * Created by peter on 19/05/18.
- */
-
-public class BasePresenter  { }
