69b9226d279f2d0bd01b79dbc00c20fb2885a3e3
shehab <shehabosama0122421@gmail.com>
2020-03-25 08:13:42 +0200

    “message”

--- app/src/main/java/com/android/two_moons_project/common/network/Urls.java
+++ app/src/main/java/com/android/two_moons_project/common/network/Urls.java
@@ -5,4 +5,5 @@ public class Urls {
     public static final String MAIN_URL ="https://shehab.develogs.com/two_moon/";
     public static final String SECOND_URL="https://shehabosama.000webhostapp.com/";
     public static final String IMAGE_URL="https://shehab.develogs.com/jobber/uploads/";
+    public String test ="";
 }
